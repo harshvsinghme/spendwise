b4d12ba332f480874fd152baeea2c4cd638e05e8
harshvsinghme <hvsreal.223@gmail.com>
2025-12-16 02:49:45 +0530

    added: husky

--- backend/src/middlewares/async-handler.ts
+++ backend/src/middlewares/async-handler.ts
@@ -1,9 +1,7 @@
-import type { Request, Response, NextFunction } from "express";
+import type { NextFunction, Request, Response } from "express";
 
 export const asyncHandler =
-  (
-    fn: (req: Request, res: Response, next: NextFunction) => Promise<unknown>
-  ) =>
+  (fn: (req: Request, res: Response, next: NextFunction) => Promise<unknown>) =>
   (req: Request, res: Response, next: NextFunction) => {
     Promise.resolve(fn(req, res, next)).catch(next);
   };
