fcfc7b61d55074b7bddcf0a487c6c937a95abc61
harshvsinghme <hvsreal.223@gmail.com>
2025-12-16 02:43:41 +0530

    added: husky

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -3,6 +3,6 @@ import healthRouter from "./health.route.js";
 
 const router: Router = Router();
 
-router.use(`/health`, healthRouter);
+router.use(`/health`, healthRouter);12
 
 export default router;
