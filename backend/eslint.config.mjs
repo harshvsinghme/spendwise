d2636322080c963fed8272f98c9335eff539f517
harshvsinghme <hvsreal.223@gmail.com>
2025-12-16 02:56:44 +0530

    added: husky

--- backend/eslint.config.mjs
+++ backend/eslint.config.mjs
@@ -1,12 +1,27 @@
 import eslint from "@eslint/js";
+import globals from "globals";
 import tseslint from "typescript-eslint";
 
-export default tseslint.config(eslint.configs.recommended, ...tseslint.configs.recommended, {
-  ignores: ["dist"],
-  rules: {
-    "@typescript-eslint/no-unused-vars": [
-      "error",
-      { argsIgnorePattern: "^_", varsIgnorePattern: "^_" },
-    ],
+export default tseslint.config(
+  eslint.configs.recommended,
+  ...tseslint.configs.recommended,
+
+  {
+    ignores: ["dist/**"],
   },
-});
+
+  {
+    languageOptions: {
+      globals: {
+        ...globals.node,
+      },
+    },
+
+    rules: {
+      "@typescript-eslint/no-unused-vars": [
+        "error",
+        { argsIgnorePattern: "^_", varsIgnorePattern: "^_" },
+      ],
+    },
+  }
+);
