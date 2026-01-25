d6de6b4b1e527ec5d2c2c4021aa4d4e7e81961d5
harshvsinghme <hvsreal.223@gmail.com>
2026-01-26 01:36:05 +0530

    feat(cors): add rate limiter middleware and update Docker command in README

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,5 +1,6 @@
 import cors from "cors";
 import express from "express";
+import { rateLimit } from "express-rate-limit";
 import { isLocal } from "./constants/index.js";
 import { httpLogger } from "./infra/logger/http-logger.js";
 import errorHandler from "./middlewares/error-handler.js";
@@ -15,6 +16,16 @@ const createApp = (): express.Application => {
       credentials: true,
     })
   );
+
+  const limiter = rateLimit({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    limit: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
+    standardHeaders: "draft-8",
+    legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
+    ipv6Subnet: 56, // Set to 60 or 64 to be less aggressive, or 52 or 48 to be more aggressive
+  });
+
+  app.use(limiter);
 
   app.use(express.json({ limit: "10kb" }));
   app.use(httpLogger);
